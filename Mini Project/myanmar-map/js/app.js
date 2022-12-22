8a10f86647ebc134e2d76cb5622fd8c398701be7
Min Htet <114550453+MinHtet007007@users.noreply.github.com>
2022-12-22 14:11:35 +0630

    Delete Mini Project/myanmar-map directory

--- Mini Project/myanmar-map/js/app.js
+++
@@ -1,8 +0,0 @@
-let currentPlace = document.querySelector(".caption-place");
-let path = document.querySelectorAll(".map path");
-
-path.forEach(x=>{
-    x.addEventListener("mouseover",_=>{
-        currentPlace.innerText = x.getAttribute('title')
-    });
-});
